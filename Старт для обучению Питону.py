2d1bcc1682a8b4a91869fbb27babc870cfb055ac
VPvpg <109874895+VPvpg@users.noreply.github.com>
2024-04-11 12:56:52 +0300

    Add files via upload

---
+++ Старт для обучению Питону.py
@@ -0,0 +1,12 @@
+#открывает две страницы с обучающими ботами ИИ
+
+import webbrowser
+import subprocess
+
+url = "https://poe.com/PythonTeacher"
+webbrowser.open(url)
+
+url = "https://poe.com/BotPython_kod"
+webbrowser.open(url)
+
+
