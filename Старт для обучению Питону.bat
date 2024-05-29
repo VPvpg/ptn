e56603d0345ae52f8d8b7b50f14a58f566bf6b77
VPvpg <109874895+VPvpg@users.noreply.github.com>
2024-05-29 15:13:50 +0300

    Update Старт для обучению Питону.bat
    Добавлена ссылка на Козу

--- Старт для обучению Питону.bat
+++ Старт для обучению Питону.bat
@@ -8,3 +8,5 @@ start "" "https://poe.com/BotPython_kod"
 start "" "https://github.com/VPvpg/ptn"
 
 start "" "https://gemini.google.com/app"
+
+start "" "https://www.coze.com/store/bot/7358700666929561617"
